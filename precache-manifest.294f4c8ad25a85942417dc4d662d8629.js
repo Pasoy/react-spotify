ceceb69d9e7a495faadbff89971dd55ef32f4b1d
Pasoy <pascal34287@gmail.com>
2020-04-20 14:39:02 +0200

    Updates

---
+++ precache-manifest.294f4c8ad25a85942417dc4d662d8629.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad31a4dfdcecbb9697108ce6a6a1ba8d",
+    "url": "/react-spotify/index.html"
+  },
+  {
+    "revision": "795940705f5c6c33a4c6",
+    "url": "/react-spotify/static/css/main.87c2f773.chunk.css"
+  },
+  {
+    "revision": "82fdeec9ef7d1023219a",
+    "url": "/react-spotify/static/js/2.28c4f1fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-spotify/static/js/2.28c4f1fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "795940705f5c6c33a4c6",
+    "url": "/react-spotify/static/js/main.5d3a521b.chunk.js"
+  },
+  {
+    "revision": "998d5565c217548a2a3a",
+    "url": "/react-spotify/static/js/runtime-main.00b90f87.js"
+  },
+  {
+    "revision": "3005871a5a18de80182ce2cf5a7167bd",
+    "url": "/react-spotify/static/media/home.3005871a.svg"
+  },
+  {
+    "revision": "d464e65a9d45f9b6b3056b0019f63a1b",
+    "url": "/react-spotify/static/media/library.d464e65a.svg"
+  },
+  {
+    "revision": "90a5d55b6a7029454dcde694fdf2bda3",
+    "url": "/react-spotify/static/media/logo.90a5d55b.svg"
+  },
+  {
+    "revision": "bf85a5d42df5192927fcedf3ccd075a2",
+    "url": "/react-spotify/static/media/play.bf85a5d4.svg"
+  },
+  {
+    "revision": "f4d57da5f8e31ac46876a65251148694",
+    "url": "/react-spotify/static/media/search.f4d57da5.svg"
+  },
+  {
+    "revision": "c147cc237b8b07e0a8875dfbbe857b29",
+    "url": "/react-spotify/static/media/spotify-circular-bold.c147cc23.woff2"
+  },
+  {
+    "revision": "6ff898ba447ac00bc6e457d25bcb0be8",
+    "url": "/react-spotify/static/media/spotify-circular.6ff898ba.woff2"
+  }
+]);
